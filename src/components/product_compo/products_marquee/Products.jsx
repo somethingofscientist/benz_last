20320d22210bf1aacd5066d001ed3246ce81e36e
abhinav-siwal <abhinav.siwal@applore.in>
2023-08-11 15:44:29 +0530

    fix : incomplete half things done

--- src/components/product_compo/products_marquee/Products.jsx
+++ src/components/product_compo/products_marquee/Products.jsx
@@ -14,60 +14,75 @@ import Traditional from "../Traditional/Aerospace";
 import { useTranslation } from "react-i18next";
 
 const Products = () => {
-  
+
   const { t, i18n } = useTranslation();
   const [page, setPage] = useState("");
+  const [containerVisible, setContainerVisible] = useState(true);
+
+  const handleCardClick = (page) => {
+    setPage(page);
+    setContainerVisible(false);
+    // window.scrollX
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const handleBackClick = () => {
+    setPage("");
+    setContainerVisible(true);
+  };
 
   return (
     <>
-      <div className={styles.product_container}>
-        <div className={styles.products_tag}>
-          <Marquee speed={200} loop={0}>
-            <p>{t("Products")} &nbsp; </p>
-            <p>{t("Products")} &nbsp; </p>
-          </Marquee>
+      {containerVisible && (
+        <div className={styles.product_container}>
+          <div className={styles.products_tag}>
+            <Marquee speed={200} loop={0}>
+              <p>{t("Products")} &nbsp; </p>
+              <p>{t("Products")} &nbsp; </p>
+            </Marquee>
+          </div>
+          <div className={styles.card_container}>
+            <div className={styles.card_up}
+              onClick={(e) => { setPage('humidity') }}
+            >
+              <div className={styles.card_text}>{t("INDUSTRIAL")}</div>
+              <div className={styles.card_subtext}>{t("Anti - Humidity")} </div>
+              <img src={pic1} alt="pic1_humidity" />
+              <div className={styles.card_icon}>
+                <HiArrowNarrowRight size={20} color="white" />
+              </div>
+            </div>
+            <div className={styles.card_down}
+              onClick={(e) => { setPage('corrosion') }}>
+              <div className={styles.card_text}>{t("INDUSTRIAL")}</div>
+              <div className={styles.card_subtext}>{t("Anti - Corrosion")} </div>
+              <img src={pic2} alt="pic1_humidity" />
+              <div className={styles.card_icon2}>
+                <HiArrowNarrowRight size={20} color="white" />
+              </div>
+            </div>
+            <div className={styles.card_up}
+              onClick={(e) => { setPage('handling') }}
+            >
+              <div className={styles.card_text}>{t("INDUSTRIAL")}</div>
+              <div className={styles.card_subtext}>{t("Handling")}</div>
+              <img src={pic3} alt="pic1_handling" />
+              <div className={styles.card_icon}>
+                <HiArrowNarrowRight size={20} color="white" />
+              </div>
+            </div>
+            <div className={styles.card_down}
+              onClick={(e) => { setPage('traditional') }}>
+              <div className={styles.card_text}>{t("INDUSTRIAL")}</div>
+              <div className={styles.card_subtext}>{t("Traditional Packaging")}</div>
+              <img src={pic4} alt="pic1_traditional" />
+              <div className={styles.card_icon2}>
+                <HiArrowNarrowRight size={20} color="white" />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.card_container}>
-          <div className={styles.card_up}
-            onClick={(e) => { setPage('humidity') }}
-          >
-            <div className={styles.card_text}>{t("INDUSTRIAL")}</div>
-            <div className={styles.card_subtext}>{t("Anti - Humidity")} </div>
-            <img src={pic1} alt="pic1_humidity" />
-            <div className={styles.card_icon}>
-              <HiArrowNarrowRight size={20} color="white" />
-            </div>
-          </div>
-          <div className={styles.card_down}
-            onClick={(e) => { setPage('corrosion') }}>
-            <div className={styles.card_text}>{t("INDUSTRIAL")}</div>
-            <div className={styles.card_subtext}>{t("Anti - Corrosion")} </div>
-            <img src={pic2} alt="pic1_humidity" />
-            <div className={styles.card_icon2}>
-              <HiArrowNarrowRight size={20} color="white" />
-            </div>
-          </div>
-          <div className={styles.card_up}
-            onClick={(e) => { setPage('handling') }}
-          >
-            <div className={styles.card_text}>{t("INDUSTRIAL")}</div>
-            <div className={styles.card_subtext}>{t("Handling")}</div>
-            <img src={pic3} alt="pic1_handling" />
-            <div className={styles.card_icon}>
-              <HiArrowNarrowRight size={20} color="white" />
-            </div>
-          </div>
-          <div className={styles.card_down}
-            onClick={(e) => { setPage('traditional') }}>
-            <div className={styles.card_text}>{t("INDUSTRIAL")}</div>
-            <div className={styles.card_subtext}>{t("Traditional Packaging")}</div>
-            <img src={pic4} alt="pic1_traditional" />
-            <div className={styles.card_icon2}>
-              <HiArrowNarrowRight size={20} color="white" />
-            </div>
-          </div>
-        </div>
-      </div>
+      )}
 
 
       {
@@ -92,6 +107,17 @@ const Products = () => {
                   <p>{t("For more Content click on our Products")} &nbsp; </p>
                 </div>
       }
+
+
+      {/* {!containerVisible && (
+        <div className={styles.product_container}>
+          {page === "humidity" && <Rust />}
+          {page === "corrosion" && <Accessories />}
+          {page === "handling" && <Handle />}
+          {page === "traditional" && <Traditional />}
+          <button onClick={handleBackClick}>Back</button>
+        </div>
+      )} */}
     </>
   );
 };
