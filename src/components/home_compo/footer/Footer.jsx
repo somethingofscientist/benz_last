2911d4326be4fba3698c595f57b4749e0b5fc226
shraddhanandapplore <shraddanand.shraddanand@rarebillions.com>
2023-09-06 12:14:06 +0530

    add news section

--- src/components/home_compo/footer/Footer.jsx
+++ src/components/home_compo/footer/Footer.jsx
@@ -12,7 +12,6 @@ import axios from "axios";
 import { HashLink } from "react-router-hash-link";
 import { useTranslation } from "react-i18next";
 
-
 const Footer = () => {
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
