6aa1d04f4947acef0bebe43a9389458d88981451
somethingofscientist <1shraddhanand@gmail.com>
2023-09-26 11:32:17 +0530

    git push

--- src/components/about_compo/MarqueeHistory/MarqueeHistory.jsx
+++ src/components/about_compo/MarqueeHistory/MarqueeHistory.jsx
@@ -17,154 +17,19 @@ const MarqueeHistory = () => {
         const { scrollTop } = event.target;
         setScrollPosition(scrollTop);
     };
+
     console.log("scrollPosition", scrollPosition)
+
+    const handleYearClick = (year) => {
+        const photoElement = document.getElementById(year);
+        console.log("year", photoElement);
+        if (photoElement) {
+            photoElement.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
         // /set according to 80% width
-        // <>
-        //     <a id="section1">
-        //         <div>
-        //             <div className={styles.products_tag}>
-        //                 <Marquee
-        //                     speed={200}
-        //                     loop={0}
-        //                 >
-        //                     <p>{t("History")} &nbsp;	 </p>
-        //                     <p>{t("History")} &nbsp;	 </p>
-        //                 </Marquee>
-        //             </div>
-        //         </div>
-        //     </a>
-
-        //     <div className={styles.product_image_container}>
-        //         <div className={styles.product_timeline}>
-
-        //             <div className={(scrollPosition == 0 && scrollPosition <= 600) ? styles.year_active : styles.year_deactivate}>
-        //                 <Link
-        //                     to="1984"
-        //                     spy={true}
-        //                     smooth={true}
-        //                     offset={-50}
-        //                     duration={500}
-        //                     className={styles.link}
-        //                 >1984
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.line_year}></div>
-        //             <div className={(scrollPosition >= 600 && scrollPosition <= 1200) ? styles.year_active : styles.year_deactivate}>
-        //                 <Link
-        //                     to="1989"
-        //                     spy={true}
-        //                     smooth={true}
-        //                     offset={-50}
-        //                     duration={500}
-        //                     className={styles.link}
-        //                 >1989
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.line_year}></div>
-        //             <div className={(scrollPosition >= 1200 && scrollPosition <= 1800) ? styles.year_active : styles.year_deactivate}>
-        //                 <Link
-        //                     to="1998"
-        //                     spy={true}
-        //                     smooth={true}
-        //                     offset={-50}
-        //                     duration={500}
-        //                     className={styles.link}
-        //                 >1998
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.line_year}></div>
-        //             <div className={(scrollPosition >= 1800 && scrollPosition <= 2400) ? styles.year_active : styles.year_deactivate}>
-        //                 <Link
-        //                     to="2005"
-        //                     spy={true}
-        //                     smooth={true}
-        //                     offset={-50}
-        //                     duration={500}
-        //                     className={styles.link}
-        //                 >2005
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.line_year}></div>
-        //             <div className={(scrollPosition >= 2400 && scrollPosition <= 3000) ? styles.year_active : styles.year_deactivate}>
-        //                 <Link
-        //                     to="2009"
-        //                     spy={true}
-        //                     smooth={true}
-        //                     offset={-50}
-        //                     duration={500}
-        //                     className={styles.link}
-        //                 >2009
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.line_year}></div>
-        //             <div className={(scrollPosition >= 3000 && scrollPosition <= 3600) ? styles.year_active : styles.year_deactivate}>
-        //                 <Link
-        //                     to="2013"
-        //                     spy={true}
-        //                     smooth={true}
-        //                     offset={-50}
-        //                     duration={500}
-        //                     className={styles.link}
-        //                 >2013
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.line_year}></div>
-        //             <div className={(scrollPosition >= 3600 && scrollPosition <= 4200) ? styles.year_active : styles.year_deactivate}>
-        //                 <Link
-        //                     to="2017"
-        //                     spy={true}
-        //                     smooth={true}
-        //                     offset={-50}
-        //                     duration={500}
-        //                     className={styles.link}
-        //                 >2017
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.line_year}></div>
-        //             <div className={(scrollPosition >= 4200 && scrollPosition <= 4800) ? styles.year_active : styles.year_deactivate}>
-        //                 <Link
-        //                     to="2018"
-        //                     spy={true}
-        //                     smooth={true}
-        //                     offset={-50}
-        //                     duration={500}
-        //                     className={styles.link}
-        //                 >2018
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.line_year}></div>
-        //             <div className={(scrollPosition >= 4800 && scrollPosition <= 5400) ? styles.year_active : styles.year_deactivate}>
-        //                 <Link
-        //                     to="2023"
-        //                     spy={true}
-        //                     smooth={true}
-        //                     offset={-50}
-        //                     duration={500}
-        //                     className={styles.link}
-        //                 >2023
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.line_year_green}></div>
-        //             <div className={styles.line_year_green_dot}></div>
-
-        //         </div>
-        //         <div className={styles.award_img} onScroll={handleScroll}>
-        //             <img id="1984" src={stamp_paper} alt="" />
-        //             <img id="1989" src={stamp_paper} alt="" />
-        //             <img id="1998" src={stamp_paper} alt="" />
-
-        //             <img id="2005" src={stamp_paper} alt="" />
-        //             <img id="2009" src={stamp_paper} alt="" />
-        //             <img id="2013" src={stamp_paper} alt="" />
-
-        //             <img id="2017" src={stamp_paper} alt="" />
-        //             <img id="2018" src={stamp_paper} alt="" />
-        //             <img id="2023" src={stamp_paper} alt="" />
-        //         </div>
-        //     </div>
-        // </>
-
 
         // set according to 50% width
         <>
@@ -193,6 +58,7 @@ const MarqueeHistory = () => {
                             offset={-50}
                             duration={500}
                             className={styles.link}
+                            onClick={() => handleYearClick("1984")}
                         >1984
                         </Link>
                     </div>
